3e94142a97d31d5e937679735f3fd83b06bc6f0e
Noel Lava <jnoel_08@yahoo.com>
2014-02-18 19:26:19 -0800

    test commit

--- trunk/Whack-A-Foe/src/com/bulalo/Sreens/GameScreen.java
+++ trunk/Whack-A-Foe/src/com/bulalo/Sreens/GameScreen.java
@@ -1,13 +1,20 @@
 package com.bulalo.Sreens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
 
 public class GameScreen implements Screen{
 
+	public GameScreen(){
+		System.out.println("GameScreen Attached");
+	}
+	
 	@Override
 	public void render(float delta) {
-		// TODO Auto-generated method stub
-		
+		// Draws the RGB color 10, 15, 230, at 100% opacity
+        Gdx.gl.glClearColor(10/255.0f, 15/255.0f, 230/255.0f, 1f);
+        Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 	}
 
 	@Override
