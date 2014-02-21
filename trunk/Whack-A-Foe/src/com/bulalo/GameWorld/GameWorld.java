b7ae5e8c25545622c00c84a496e9483546c551fd
Allen Arcenal <allenarcenal@gmail.com>
2014-02-21 14:50:02 +0800

    Dummy.java -> spawn() GameWorld.java -> random coordinate

--- trunk/Whack-A-Foe/src/com/bulalo/GameWorld/GameWorld.java
+++ trunk/Whack-A-Foe/src/com/bulalo/GameWorld/GameWorld.java
@@ -1,13 +1,24 @@
 //This Class is the one responsible for updating the game objects to the game screen. This class will be later on called in the gameScreen class 
 package com.bulalo.GameWorld;
 
+import java.util.Random;
+
 import com.bulalo.GameObjects.Dummy;
 
 public class GameWorld {
 	private Dummy dummy;
 	
 	public GameWorld(){
-		dummy = new Dummy(500, 65, 110, 33, 48);
+		Random randX = new Random();
+		Random randY = new Random();
+		int x,y;
+		int[] coordinateX = {30,65,100} ;
+		int[] coordinateY = {83,128,180};		
+		x = randX.nextInt(2) + 1;
+		y = randY.nextInt(2) + 1;
+		
+		dummy = new Dummy(500, x, y, 33, 48);
+		dummy.spawn(coordinateX[x], coordinateY[y]);
 		
 	}
 	
